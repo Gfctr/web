3ecb30140e29ab255fbf17f2b2d9a580ff6f067c
Gabriel Rios <gfctrios@gmail.com>
2022-06-19 21:19:21 -0300

    Create index.js

---
+++ maquina-de-escrever/index.js
@@ -0,0 +1,17 @@
+var content = 'Texto';
+var text = document.getElementById('text');
+var speed = 190;
+var cont = 0;
+
+function typeWriter() {
+    if (cont < content.length) {
+        text.textContent += content.charAt(cont);
+        cont++;
+        setTimeout(typeWriter, speed);
+    } else {
+        text.textContent = '';
+        cont = 0;
+        typeWriter();
+    }
+}
+typeWriter();
